47bf54875caa34faebbb7ba5b7d10ec612d3baee
okimurak <okimurak0901@gmail.com>
2021-11-18 17:42:30 +0900

    feat: add eksctl completer

--- tool/install_completer.sh
+++ tool/install_completer.sh
@@ -5,6 +5,10 @@
 autoload bashcompinit && bashcompinit
 autoload -Uz compinit && compinit
 
-## Docker
+## eksctl
+mkdir -p ~/.zsh/completion/
+eksctl completion zsh > ~/.zsh/completion/_eksctl
+
+## docker-compose
 mkdir -p ~/.zsh/completion
 curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/zsh/_docker-compose > ~/.zsh/completion/_docker-compose
