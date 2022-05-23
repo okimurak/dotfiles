d76a09c048b79f19dc1767ff8509a54cf3cabddf
okimurak <okimurak0901@gmail.com>
2022-05-23 19:15:09 +0900

    refactor: fix path

--- script/wsl.sh
+++ script/wsl.sh
@@ -14,12 +14,16 @@ EOF
   fi
   cd ${configure_path} || exit
 
+  local SystemMountPath="/mnt/c"
+
+  # certutil
+  ln -s "${SystemMountPath}/Windows/System32/certutil.exe"
   # clip
-  ln -s "/mnt/c/Windows/System32/clip.exe"
+  ln -s "${SystemMountPath}/Windows/System32/clip.exe"
   # Docker Desktop for Windows
-  ln -s "/mnt/c/Program Files/Docker/Docker/resources/bin/docker-credential-desktop.exe"
+  ln -s "${SystemMountPath}/Program Files/Docker/Docker/resources/bin/docker-credential-desktop.exe"
   # PowerShell
-  ln -s "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
+  ln -s "${SystemMountPath}/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
 }
 
 uninstall() {
