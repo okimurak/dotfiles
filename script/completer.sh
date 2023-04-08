9211fdb6022ed0f7a88dc70db34e851dbb962aeb
okimurak <46171497+okimurak@users.noreply.github.com>
2023-04-08 14:35:52 +0000

    fix: completer

--- script/completer.sh
+++ script/completer.sh
@@ -1,29 +1,35 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 install() {
   # Replace install
-  workspace="$(dirname $0)"/..
-
-  rm -rf "${workspace}/.zsh/completion/*"
+  workspace="$(dirname $0)"
+  completion_path=${workspace}/.zsh/completion
+  if [[ ! -d "${completion_path}" ]]; then
+    mkdir -p "${completion_path}"
+    chmod 755 ${workspace}/.zsh/
+    chmod 755 ${completion_path}
+  else
+    rm -rf "${completion_path}/*"
+  fi
 
   ## docker, docker-compose
-  curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker >"${workspace}/.zsh/completion/_docker"
+  curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker >"${completion_path}/_docker"
 
   ## eksctl
-  eksctl completion zsh >"${workspace}/.zsh/completion/_eksctl"
+  eksctl completion zsh >"${completion_path}/_eksctl"
 
   ## Git
-  curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash >"${workspace}/.zsh/completion/git-completion.bash"
-  curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh >"${workspace}/.zsh/completion/_git"
+  curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash >"${completion_path}/git-completion.bash"
+  curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh >"${completion_path}/_git"
 
   ## helm
-  helm completion zsh >"${workspace}/.zsh/completion/_helm"
+  helm completion zsh >"${completion_path}/_helm"
 
   ## kubectl
-  kubectl completion zsh >"${workspace}/.zsh/completion/_kubectl"
+  kubectl completion zsh >"${completion_path}/_kubectl"
 
   ## yq
-  yq shell-completion zsh >"${workspace}/.zsh/completion/_yq"
+  yq shell-completion zsh >"${completion_path}/_yq"
 
   source "${workspace}/.zshrc"
 }
